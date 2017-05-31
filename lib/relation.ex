675ae2a54deff322322aa559a15c753b23f8e004
wangyubin <linuxr@aliyun.com>
2017-05-31 10:30:52 +0800

    init commit

---
+++ lib/relation.ex
@@ -0,0 +1,18 @@
+defmodule Relation do
+  @moduledoc """
+  Documentation for Relation.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Relation.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/relation_test.exs
@@ -0,0 +1,8 @@
+defmodule RelationTest do
+  use ExUnit.Case
+  doctest Relation
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
